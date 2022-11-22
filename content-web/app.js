a21e3cff535f6e1267eb0f8dc21f50150ffc8366
Alec Dunn <116566143+ADunn-Insight@users.noreply.github.com>
2022-11-22 16:42:23 -0500

    Update app.js
    take out the limiter to see if that fixes the error on step 6

--- content-web/app.js
+++ content-web/app.js
@@ -70,14 +70,14 @@ app.get('/api/stats', function (req, res) {
 
 //copied from suggested solution in codeql
 // set up rate limiter: maximum of five requests per minute
-var RateLimit = require('express-rate-limit');
-var limiter = new RateLimit({
-  windowMs: 1*60*1000, // 1 minute
-  max: 5
-});
+//var RateLimit = require('express-rate-limit');
+//var limiter = new RateLimit({
+//  windowMs: 1*60*1000, // 1 minute
+//  max: 5
+//});
 
 // apply rate limiter to all requests
-app.use(limiter);
+//app.use(limiter);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/content-web/index.html'));
