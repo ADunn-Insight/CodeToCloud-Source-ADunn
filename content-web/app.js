621e53424115badffdef9b77e152f9f0a4f800df
Alec Dunn <116566143+ADunn-Insight@users.noreply.github.com>
2022-11-22 16:18:53 -0500

    Update app.js
    added rate limiter

--- content-web/app.js
+++ content-web/app.js
@@ -68,7 +68,16 @@ app.get('/api/stats', function (req, res) {
   });
 });
 
+//copied from suggested solution in codeql
+// set up rate limiter: maximum of five requests per minute
+var RateLimit = require('express-rate-limit');
+var limiter = new RateLimit({
+  windowMs: 1*60*1000, // 1 minute
+  max: 5
+});
 
+// apply rate limiter to all requests
+app.use(limiter);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/content-web/index.html'));
