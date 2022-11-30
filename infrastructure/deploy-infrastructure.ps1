e85818fd0d369d5273b67aa4e03cbd8c810c2505
Alec Dunn <116566143+ADunn-Insight@users.noreply.github.com>
2022-11-30 23:37:04 +0000

    added cosmosDB to script AB#37 #45

--- infrastructure/deploy-infrastructure.ps1
+++ infrastructure/deploy-infrastructure.ps1
@@ -9,3 +9,11 @@ $location2 = "northeurope"
 
 #create the rss group
 az group create -l $location1 -n $resourcegroupName
+
+#create the cosmosDB
+az cosmosdb create --name $cosmosDBName \
+--resource-group $resourcegroupName \
+--locations regionName=$location1 failoverPriority=0 isZoneRedundant=False \
+--locations regionName=$location2 failoverPriority=1 isZoneRedundant=True \
+--enable-multiple-write-locations \
+--kind MongoDB 
