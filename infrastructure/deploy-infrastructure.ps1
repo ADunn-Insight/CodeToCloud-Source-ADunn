6cadb0f0ed0293adf8e50ffdae377fc736e53ac0
Alec Dunn <116566143+ADunn-Insight@users.noreply.github.com>
2022-11-30 14:44:50 +0000

    folder and script for creating rss group AB#43

---
+++ infrastructure/deploy-infrastructure.ps1
@@ -0,0 +1,11 @@
+#setup variables
+$studentprefix = "add"
+$resourcegroupName = "fabmedical-rg-" + $studentprefix
+$cosmosDBName = "fabmedical-cdb-" + $studentprefix
+$webappName = "fabmedical-web-" + $studentprefix
+$planName = "fabmedical-plan-" + $studentprefix
+$location1 = "westeurope"
+$location2 = "northeurope"
+
+#create the rss group
+az group create -l $location1 -n $resourcegroupName
