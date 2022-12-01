8b813760554f66b138793c4a8463dc5257fb6459
Alec Dunn <116566143+ADunn-Insight@users.noreply.github.com>
2022-12-01 01:07:40 +0000

    fix region issue AB#37 AB#45

--- infrastructure/deploy-infrastructure.ps1
+++ infrastructure/deploy-infrastructure.ps1
@@ -4,20 +4,19 @@ $resourcegroupName = "fabmedical-rg-" + $studentprefix
 $cosmosDBName = "fabmedical-cdb-" + $studentprefix
 $webappName = "fabmedical-web-" + $studentprefix
 $planName = "fabmedical-plan-" + $studentprefix
-$location1 = "westeurope"
-$location2 = "northeurope"
+$location1 = "eastus2"
+$location2 = "centralus"
 
 #create the rss group
 az group create -l $location1 -n $resourcegroupName
 
 #create the cosmosDB
-#az cosmosdb create --name $cosmosDBName `
-#--resource-group $resourcegroupName `
-#--locations regionName=$location1 failoverPriority=0 isZoneRedundant=False `
-#--locations regionName=$location2 failoverPriority=1 isZoneRedundant=True `
-#--enable-multiple-write-locations `
-#--kind MongoDB 
-#commenting out for now while deciding on a backup region
+az cosmosdb create --name $cosmosDBName `
+--resource-group $resourcegroupName `
+--locations regionName=$location1 failoverPriority=0 isZoneRedundant=False `
+--locations regionName=$location2 failoverPriority=1 isZoneRedundant=True `
+--enable-multiple-write-locations `
+--kind MongoDB 
 
 #create the App Service Plan
 az appservice plan create --name $planName --resource-group $resourcegroupName --sku S1 --is-linux
