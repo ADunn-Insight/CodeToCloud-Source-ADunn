80629d1e9a4f258bd3509ef8d0d02a2663946268
Alec Dunn <116566143+ADunn-Insight@users.noreply.github.com>
2022-12-02 21:15:47 +0000

    configure with log analytics workspace AB#39

--- infrastructure/setup-appinsights.ps1
+++ infrastructure/setup-appinsights.ps1
@@ -4,6 +4,8 @@ $location1 = "westus3"
 $appInsights = "fabmedicalai-" + $studentsuffix
 
 az extension add --name application-insights
-$ai = az monitor app-insights component create --app $appInsights --location $location1 --kind web -g $resourcegroupName --application-type web --retention-time 120 | ConvertFrom-Json
+$ai = az monitor app-insights component create --app $appInsights --location $location1 --kind web -g $resourcegroupName `
+    --workspace "/subscriptions/93e5af0e-7bce-4a98-8604-745ed736a73f/resourceGroups/fabmedical-rg-add/providers/Microsoft.OperationalInsights/workspaces/fabmedical-law-add" `
+    --application-type web | ConvertFrom-Json
 
 Write-Host "AI Instrumentation Key=$($ai.instrumentationKey)"
