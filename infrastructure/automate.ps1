4e521687cd07616a13896aefff269be60fe24bb5
Alec Dunn <116566143+ADunn-Insight@users.noreply.github.com>
2022-12-07 21:53:55 +0000

    properly call scripts

--- infrastructure/automate.ps1
+++ infrastructure/automate.ps1
@@ -1,4 +1,4 @@
 #fully automate the creation and deployment of azure resources with one script
-& $deploy-infrastructure.ps1
-& $setup-appinsights.ps1
-& $deploy-container.ps1
+& .\deploy-infrastructure.ps1
+& .\setup-appinsights.ps1
+& .\deploy-container.ps1
