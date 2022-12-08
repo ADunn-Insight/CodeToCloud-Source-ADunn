d83ab549ae2f82d18f3d423951a20c042bad92b2
Alec Dunn <116566143+ADunn-Insight@users.noreply.github.com>
2022-12-08 18:31:07 +0000

    add aiInstKey to app.js

--- infrastructure/automate.ps1
+++ infrastructure/automate.ps1
@@ -1,4 +1,9 @@
 #fully automate the creation and deployment of azure resources with one script
 & .\deploy-infrastructure.ps1
 & .\setup-appinsights.ps1
+$insertString = "appInsights.setup(" + $aiInstKey + ");"
+(Get-Content ../content-web/app.js) -Replace "appInsights\.setup\(\`"\S+\`"\);", $insertString | Set-Content ../content-web/app.js
+git add ../content-web/app.js
+git commit -m "added new aiInstKey to app.js"
+git push
 & .\deploy-container.ps1
