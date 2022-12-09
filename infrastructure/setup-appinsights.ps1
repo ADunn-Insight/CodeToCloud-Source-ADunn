a6b7e754dd8e63d09067ffaee64db8696f87e2a0
Alec Dunn <116566143+ADunn-Insight@users.noreply.github.com>
2022-12-09 14:05:53 +0000

    link app insights to web app w/ default values

--- infrastructure/setup-appinsights.ps1
+++ infrastructure/setup-appinsights.ps1
@@ -13,3 +13,20 @@ $ai = az monitor app-insights component create --app $appInsights --location $lo
     --application-type web | ConvertFrom-Json
 
 $global:aiInstKey = $ai.instrumentationKey
+
+#link the newly created app insights to the webapp with default values
+az webapp config appsettings set --resource-group $resourceGroupName `
+--name $webappName `
+--settings APPINSIGHTS_INSTRUMENTATIONKEY=$global:aiInstKey `
+    APPINSIGHTS_PROFILERFEATURE_VERSION=1.0.0 `
+    APPINSIGHTS_SNAPSHOTFEATURE_VERSION=1.0.0 `
+    APPLICATIONINSIGHTS_CONNECTION_STRING=$ai.ConnectionString `
+    ApplicationInsightsAgent_EXTENSION_VERSION=~2 `
+    DiagnosticServices_EXTENSION_VERSION=~3 `
+    InstrumentationEngine_EXTENSION_VERSION=disabled `
+    SnapshotDebugger_EXTENSION_VERSION=disabled `
+    XDT_MicrosoftApplicationInsights_BaseExtensions=disabled `
+    XDT_MicrosoftApplicationInsights_Mode=recommended `
+    XDT_MicrosoftApplicationInsights_PreemptSdk=disabled
+
+
