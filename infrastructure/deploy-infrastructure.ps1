e0fd1d6e8100d2390e78924697fdd2087a837a6f
Alec Dunn <116566143+ADunn-Insight@users.noreply.github.com>
2022-12-12 15:48:00 +0000

    add try-finally for rg creation

--- infrastructure/deploy-infrastructure.ps1
+++ infrastructure/deploy-infrastructure.ps1
@@ -11,47 +11,50 @@ $manipulate = ""
 $dbKeys = ""
 
 #create the rss group
-az group create -l $location1 -n $resourcegroupName
+try {
+    az group create -l $location1 -n $resourcegroupName    
+}
+finally {
+    #create the cosmosDB
+    az cosmosdb create --name $cosmosDBName `
+    --resource-group $resourcegroupName `
+    --locations regionName=$location1 failoverPriority=0 isZoneRedundant=False `
+    --locations regionName=$location2 failoverPriority=1 isZoneRedundant=True `
+    --enable-multiple-write-locations `
+    --kind MongoDB `
+    --server-version 4.0
 
-#create the cosmosDB
-az cosmosdb create --name $cosmosDBName `
---resource-group $resourcegroupName `
---locations regionName=$location1 failoverPriority=0 isZoneRedundant=False `
---locations regionName=$location2 failoverPriority=1 isZoneRedundant=True `
---enable-multiple-write-locations `
---kind MongoDB `
---server-version 4.0
+    #create the App Service Plan
+    az appservice plan create --name $planName --resource-group $resourcegroupName --sku S1 --is-linux
 
-#create the App Service Plan
-az appservice plan create --name $planName --resource-group $resourcegroupName --sku S1 --is-linux
+    #get and configure dbConnection string
+    $dbKeys = az cosmosdb keys list -n $cosmosDBName -g $resourcegroupName --type connection-strings `
+        --query "connectionStrings[?description=='Primary MongoDB Connection String'].connectionString"
+    $manipulate = $dbKeys[1]
+    $manipulate = $manipulate.Split("""")[1]
+    $manipulate = $manipulate.Split("?")
+    $dbConnection = $manipulate[0] + "contentdb?" + $manipulate[1]
 
-#get and configure dbConnection string
-$dbKeys = az cosmosdb keys list -n $cosmosDBName -g $resourcegroupName --type connection-strings `
-    --query "connectionStrings[?description=='Primary MongoDB Connection String'].connectionString"
-$manipulate = $dbKeys[1]
-$manipulate = $manipulate.Split("""")[1]
-$manipulate = $manipulate.Split("?")
-$dbConnection = $manipulate[0] + "contentdb?" + $manipulate[1]
+    #create the WebApp with nginx
+    az webapp create --resource-group $resourcegroupName `
+    --plan $planName --name $webappName -i nginx
 
-#create the WebApp with nginx
-az webapp create --resource-group $resourcegroupName `
---plan $planName --name $webappName -i nginx
+    #configure the webapp settings
+    az webapp config container set `
+    --docker-registry-server-password $CR_PAT `
+    --docker-registry-server-url https://ghcr.io `
+    --docker-registry-server-user notapplicable `
+    --multicontainer-config-file ../docker-compose.yml `
+    --multicontainer-config-type COMPOSE `
+    --name $webappName `
+    --resource-group $resourcegroupName `
+    --enable-app-service-storage true
 
-#configure the webapp settings
-az webapp config container set `
---docker-registry-server-password $CR_PAT `
---docker-registry-server-url https://ghcr.io `
---docker-registry-server-user notapplicable `
---multicontainer-config-file ../docker-compose.yml `
---multicontainer-config-type COMPOSE `
---name $webappName `
---resource-group $resourcegroupName `
---enable-app-service-storage true
+    #set the mongoDB connection
+    az webapp config appsettings set --resource-group $resourceGroupName `
+    --name $webappName `
+    --settings MONGODB_CONNECTION=$dbConnection
 
-#set the mongoDB connection
-az webapp config appsettings set --resource-group $resourceGroupName `
---name $webappName `
---settings MONGODB_CONNECTION=$dbConnection
-
-#populate the db
-docker run -ti -e MONGODB_CONNECTION=$dbConnection ghcr.io/adunn-insight/fabrikam-init
+    #populate the db
+    docker run -ti -e MONGODB_CONNECTION=$dbConnection ghcr.io/adunn-insight/fabrikam-init
+}
