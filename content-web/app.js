e1fb9a0e765e74dc189481bd363fb41b7d3d8fdd
Alec Dunn <alec.dunn@insight.com>
2022-12-16 13:14:36 +0000

    added new aiInstKey to app.js

--- content-web/app.js
+++ content-web/app.js
@@ -6,7 +6,7 @@ const request = require('request');
 const app = express();
 
 const appInsights = require("applicationinsights");
-appInsights.setup("0cfc6233-c088-478d-9ba8-04ebd4890722");
+appInsights.setup("140e321e-80e4-4e29-bf70-7a51f74a8d1f");
 appInsights.start();
 
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
